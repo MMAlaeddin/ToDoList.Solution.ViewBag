d790ef8374d6068d7c082f80b7999eb3b1b6d9b4
Student <example@example.com>
2020-03-18 14:04:14 -0700

    recreated homework. forgot to commit, but we've been STRUGGLIN! no time to commit
    Co-authored-by: Steph Podolak <stephpodolak@gmail.com>
    Co-authored-by: Mariam Alaeddin <mariamalaeddin@gmail.com>

--- ToDoList/Controllers/ItemController.cs
+++ ToDoList/Controllers/ItemController.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace ToDoList.Controllers
 {
@@ -17,8 +19,7 @@ namespace ToDoList.Controllers
 
     public ActionResult Index()
     {
-      // gets Items table
-      List<Item> model = _db.Items.ToList(); // ToList from Linq - replaces GetAll() method
+      List<Item> model = _db.Items.Include(items => items.Category).ToList();
       return View(model);
     }
 
@@ -39,6 +40,33 @@ namespace ToDoList.Controllers
     {
       Item thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id); //Find()
       return View(thisItem);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+      return View(thisItem);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Item item)
+    {
+       _db.Entry(item).State = EntityState.Modified;
+       _db.SaveChanges();
+       return RedirectToAction("Index");
+    }
+    public ActionResult Delete(int id)
+    {
+      var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+      return View(thisItem);
+    }
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+      _db.Items.Remove(thisItem);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   } 
 }
--- ToDoList/Models/ToDoListContext.cs
+++ ToDoList/Models/ToDoListContext.cs
@@ -5,7 +5,7 @@ namespace ToDoList.Models
 {
   public class ToDoListContext : DbContext // db represents an instance of this class (DbClass)
   {
-    // public virtual DbSet<Category> Categories { get; set; }
+    public virtual DbSet<Category> Categories { get; set; }
     public DbSet<Item> Items { get; set; } //represents Items table in out to_do_list database
     
     public ToDoListContext(DbContextOptions options) : base(options) { }
